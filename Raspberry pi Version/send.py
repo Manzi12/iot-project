74eb8470d2fdf006d0b81e428f8212fda2e27530
Manzi Joseph <35697102+Manzi12@users.noreply.github.com>
2018-05-12 23:16:29 +0100

    Update send.py

--- Raspberry pi Version/send.py
+++ Raspberry pi Version/send.py
@@ -1,14 +1,14 @@
 # Import library and create instance of REST client.
 from Adafruit_IO import Client
 from time import sleep
-aio = Client('ADAFRUIT KEY')
+aio = Client('adafruit key')
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13,GPIO.OUT)
 my_pwm=GPIO.PWM (13, 1000)
 
-# Add the value 98.6 to the feed 'Temperature'.
+# Add the value 70 to the feed 'Speed'.
 my_pwm.start(60)
 while True:
     for i in range(70,100):
@@ -17,4 +17,3 @@ while True:
         speed = i
         print('speed: {0}'.format(speed))
         aio.send('speed', i)
-
